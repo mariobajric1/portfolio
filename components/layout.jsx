eb8ef9ab4b49523f891730a604eb3e6c6898e2b7
Mario <Mario@Jadrankos-iMac.local>
2020-09-07 18:42:18 -0500

    readme

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,15 +3,11 @@ import Link from "next/link";
 import styles from "../styles/layout.module.css";
 import Nav from "./Nav";
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <div className={styles.layout__container}>
-        <Nav />
-        <main className={styles.main}>{children}</main>
-        <footer className={styles.footer}></footer>
-      </div>
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <div className={styles.layout__container}>
+    <Nav />
+    <main className={styles.main}>{children}</main>
+    <footer className={styles.footer} />
+  </div>
+);
 export default Layout;
