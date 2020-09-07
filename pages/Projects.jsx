58694752ae15d8058c84df27cb793ef3f6e8fbe7
Mario <Mario@Jadrankos-iMac.local>
2020-09-07 18:59:53 -0500

    editcards

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -33,17 +33,17 @@ const ProjectsPage = () => {
       </div>
       <div className={styles.projectCards}>
         <ProjectCard
-          src="https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg"
+          src="https://morningtick.com/wp-content/uploads/2020/03/Amazon-will-allow-voice-searches-from-now-on.jpg"
           title="Amazon Clone"
           description="React-App powered by Google's Search API"
         />
         <ProjectCard
-          src="https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg"
+          src="https://play-lh.googleusercontent.com/aZrXAunkovhf0630Ykz1A7h2rzFX_dErd6fRiB7fNKU_DkNtetTquEra1bjc3sR2kLs"
           title="Whats App Clone"
           description="React-App powered by Google's Search API"
         />
         <ProjectCard
-          src="https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg"
+          src="https://logos-world.net/wp-content/uploads/2020/04/Facebook-Logo-2019-present.jpg"
           title="Facebook Clone"
           description="React-App powered by Google's Search API"
         />
