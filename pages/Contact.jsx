b603b0638fd427837b2e992b6d7be213dfbf2e18
Mario <Mario@Jadrankos-iMac.local>
2020-09-23 12:10:17 -0500

    contact

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,42 +1,20 @@
-import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/layout.module.css";
 
-const ContactPage = () => {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  };
-
-  return (
-    <>
-      <Head>
-        <title>Contact Me</title>
-      </Head>
-      <div>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-        <h2 className={styles.pageTitle}>Contact Me</h2>
-      </div>
-    </>
-  );
-};
+const ContactPage = () => (
+  <>
+    <Head>
+      <title>Contact Me</title>
+    </Head>
+    <div>
+      <h2 className={styles.pageTitle}>Contact Me</h2>
+      <h3 className={styles.pageTitle}>(847) 373-6323</h3>
+      <h3 className={styles.pageTitle}>mariobajric1@gmail.com</h3>
+      <a href="https://github.com/mariobajric1">
+        <h3 className={styles.pageTitle}>https://github.com/mariobajric1</h3>
+      </a>
+    </div>
+  </>
+);
 
 export default ContactPage;
