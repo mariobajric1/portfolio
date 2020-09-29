b1a0241ae335565f99a27e48f759f0e54c74fa98
Mario <Mario@Jadrankos-iMac.local>
2020-09-29 09:13:43 -0500

    project cards

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -20,7 +20,7 @@ const ProjectsPage = () => {
             href="https://clone-5df57.web.app/"
             src="https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg"
             title="Google Clone"
-            description="React-App powered by Google's Search API"
+            description="React-App powered by Google's Search API. Hosted by Firebase"
             gitLink="https://github.com/mariobajric1/google-clone"
           />
 
@@ -28,7 +28,7 @@ const ProjectsPage = () => {
             href="https://amzn-65993.web.app/"
             src="https://morningtick.com/wp-content/uploads/2020/03/Amazon-will-allow-voice-searches-from-now-on.jpg"
             title="Amazon Clone"
-            description="React-App powered by Google's Search API"
+            description="E-Commerce Amazon-Styled React-App powered by the Stripe Payments API. Hosted By Firebase"
             gitLink="https://github.com/mariobajric1/amazon-clone"
           />
           <ProjectCard
@@ -41,16 +41,16 @@ const ProjectsPage = () => {
 
         <div className={styles.projectCards}>
           <ProjectCard
-            src="https://play-lh.googleusercontent.com/aZrXAunkovhf0630Ykz1A7h2rzFX_dErd6fRiB7fNKU_DkNtetTquEra1bjc3sR2kLs"
-            title="Whats App Clone"
-            description="React-App powered by Google's Search API"
+            src="/chat.jpg"
+            title="Chat App"
+            description="Chat App powered by a Mongo Database."
             inProg={true}
           />
 
           <ProjectCard
             src="/eth.jpg"
             title="Scafforld-Eth"
-            description="Ethereum Web Wallet"
+            description="React and Solidity Ethereum Web Wallet with Recovery."
             inProg={true}
           />
           {/* <ProjectCard
