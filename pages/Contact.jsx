d5b86d59e56a3ebf1a89c5e5e04bbe473580f04f
Mario <Mario@Jadrankos-iMac.local>
2020-10-12 12:33:29 -0500

    video

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,11 +1,21 @@
+import React from "react";
 import BusinessCard from "../components/BusinessCard";
+import ReactPlayer from "react-player"
+import styles from '../styles/layout.module.css';
 
 const ContactPage = () => (
   <>
-    <div>
       <BusinessCard src="./me.png" />
-    </div>
+      <div className={styles.yt__video}>
+        <ReactPlayer
+          control
+          url="https://www.youtube.com/watch?v=RGY8XUq7oBE&t=411s"
+        />
+      </div>
   </>
 );
 
 export default ContactPage;
+
+
+
